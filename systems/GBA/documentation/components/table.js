705cfde9ccf370bdce0567cae7805c5e0e2cad0e
HDR <refshet@gmail.com>
2018-01-02 21:19:37 +0100

    nope

--- systems/GBA/documentation/components/table.js
+++ systems/GBA/documentation/components/table.js
@@ -1,17 +1,21 @@
 function AGBFunction() {
-  // Declare variables 
   var input, filter, table, tr, td, i;
   input = document.getElementById("AGBInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("AGBTable");
   tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td");
-    if (td) {
-        tr[i].style.display = "";
-      }
-    } 
-  }
+  var data = "";
+  
+  rows.forEach(function(row) {
+  
+    Array.prototype.slice.call(row.getElementsByTagName("td")).forEach(function(r){
+      data += r.textContent;  
+    });
+	
+	if(data.toUpperCase().indexOf(filter) > - 1){
+		row.classList.remove("hidden");
+	} else {
+		row.classList.add("hidden");
+	}
+  });
 }
