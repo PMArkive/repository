6be428d7939546fe2ffca3b979279250ad84f1a0
HDR <refshet@gmail.com>
2018-01-02 21:08:16 +0100

    javascriptsucks

--- systems/GBA/documentation/components/table.js
+++ systems/GBA/documentation/components/table.js
@@ -10,7 +10,7 @@ function AGBFunction() {
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td");
     if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > 1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
