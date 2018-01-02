e399c4648604ef1faab70018a248e7610b4488db
HDR <refshet@gmail.com>
2018-01-02 21:22:43 +0100

    god why

--- systems/GBA/documentation/components/table.js
+++ systems/GBA/documentation/components/table.js
@@ -1,21 +1,39 @@
+var input = document.getElementById("AGBInput");
+var table = document.getElementById("AGBTable");
+
+// Do event binding in JavaScript, not HTML
+input.addEventListener("keyup", filter);
+input.addEventListener("search", filter);
+
+// Get all rows, except the header and convert to array so .forEach() can be used to loop
+var rows = Array.prototype.slice.call(table.querySelectorAll("tr:not(.header)"));  
+
 function AGBFunction() {
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("AGBInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("AGBTable");
-  tr = table.getElementsByTagName("tr");
-  var data = "";
-  
+  // Always trim user input
+  var filter = input.value.trim().toUpperCase();
+
+  // Loop the rows
   rows.forEach(function(row) {
   
+    // You really don't need to know if the search criteria
+    // is in the first or second cell. You only need to know
+    // if it is in the row.
+    var data = "";
+    // Loop over all the cells in the current row and concatenate their text
     Array.prototype.slice.call(row.getElementsByTagName("td")).forEach(function(r){
+      // Don't use .innerHTML unless there is HTML. Use textContent when there isn't.
       data += r.textContent;  
     });
-	
-	if(data.toUpperCase().indexOf(filter) > - 1){
-		row.classList.remove("hidden");
-	} else {
-		row.classList.add("hidden");
-	}
+
+    // Check the string for a match and show/hide row as needed
+    // Don't set individual styles. Add/remove classes instead
+    if(data.toUpperCase().indexOf(filter) > -1){
+      // show row
+      row.classList.remove("hidden");
+    } else {
+      // hide row
+      row.classList.add("hidden");
+    }
   });
+  
 }
